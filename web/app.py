b37d881d4ba03ef4d1bb49f2df3c95b7945348f4
McDaisy <gmakdessi@gmail.com>
2022-07-27 01:32:10 +0200

    Update app.py

--- web/app.py
+++ web/app.py
@@ -163,7 +163,6 @@ def reboot():
 @app.route('/update', methods = ['GET', 'POST'])
 def update():
     os.system('bash -c "sleep 1; updateroot"')
-    return redirect('/')
 
 @app.route('/updatestatus', methods = ['GET', 'POST'])
 def updatestatus():
